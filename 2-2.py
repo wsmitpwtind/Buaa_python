6ff4cff561fec976814495f14a244fd970bf134e
wsmitpwtind <19376075@buaa.edu.cn>
2022-07-11 18:46:34 +0800

    initial

--- 2-2.py
+++
@@ -1,6 +0,0 @@
-a = input()
-b = input()
-if a.count(b) == 0:
-    print("False")
-else:
-    print(a.count(b))
---
+++ main.py
@@ -0,0 +1,5 @@
+print("hi, everyone".title())
+my_name = "My name is bob."
+print(my_name.title())
+print("abcd efg")
+print("abcd efg".title())
