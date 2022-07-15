a10185ffcd85c50ebec2d0b472d209d3baddd383
wsmitpwtind <19376075@buaa.edu.cn>
2022-07-15 11:30:36 +0800

    ini

--- 3-1.py
+++
@@ -1,6 +0,0 @@
-temp = input().split(" ")
-temp = list(set(temp))
-temp.sort()
-for i in range(len(temp) - 1):
-    print(temp[i], end=" ")
-print(temp[len(temp) - 1])
--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-a, b = "123", "2"
-print(a.count(b))
+import tkinter
+
+root = tkinter.Tk()
+root.title('<问卷调查>')
+label1 = tkinter.Label(root, text = "plz answer")
+label2 = tkinter.Label(root, text = "1. which part mest dicficolt")
+label1.pack()
+label2.pack()
+root.mainloop()
